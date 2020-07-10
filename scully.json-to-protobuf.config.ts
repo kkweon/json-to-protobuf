0a9fbca522410aca2c5ef5d19ad552af53a94189
Mo Kweon <kkweon@google.com>
2020-07-10 01:23:59 -0700

    feat: add Scully

---
+++ scully.json-to-protobuf.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "json-to-protobuf",
+  outDir: './dist/static',
+  routes: {
+  }
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { ScullyLibModule } from '@scullyio/ng-lib'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ScullyLibModule],
   providers: [],
   bootstrap: [AppComponent],
 })
