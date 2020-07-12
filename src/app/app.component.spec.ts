fbe96fb1ec36218b9f76ef85cd7a93ba77dc13b8
Mo Kweon <kkweon@google.com>
2020-07-11 21:34:12 -0700

    fix: format and test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,17 +2,20 @@ import { TestBed, async } from '@angular/core/testing'
 import { RouterTestingModule } from '@angular/router/testing'
 import { AppComponent } from './app.component'
 
+let fixture
+let component
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [AppComponent],
     }).compileComponents()
+
+    fixture = TestBed.createComponent(AppComponent)
+    component = fixture.componentInstance
   }))
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    const app = fixture.componentInstance
-    expect(app).toBeTruthy()
+    expect(component).toBeTruthy()
   })
 })
